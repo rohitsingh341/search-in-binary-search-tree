106e8a3a84d87376ea082cfd5e6bf7d628ceeec0
rohitsingh341 <rohitsingh341@gmail.com>
2022-10-10 18:28:52 +0100

    Searching a node in BST

---
+++ src/com/rohit/dsa/SearchInBinarySearchTree.java
@@ -0,0 +1,53 @@
+package com.rohit.dsa;
+
+public class SearchInBinarySearchTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+
+        TreeNode leftNode = new TreeNode(1);
+        root.left = leftNode;
+
+        TreeNode rightNode = new TreeNode(3);
+        root.right = rightNode;
+
+        TreeNode rightOfRightNode = new TreeNode(4);
+        rightNode.right = rightOfRightNode;
+
+        int targetValue1 = 5;
+        int targetValue2 = 4;
+
+        System.out.println("Target value " + targetValue1 + "exists in Binary Tree -> " + search(root, targetValue1)); // 5 does not exist
+        System.out.println("Target value " + targetValue2 + "exists in Binary Tree -> " + search(root, targetValue2)); // 4 exists
+    }
+
+
+    // Time Complexity - O(logn) because half of the side we are avoiding by having BST as the data container.
+    // BST should be balanced having 1 node difference is okay
+    public static boolean search(TreeNode root, int targetValue) {
+        if (root == null) {
+            return false;
+        }
+        else if (targetValue > root.val) {
+            return search(root.right, targetValue);
+        }
+        else if (targetValue < root.val) {
+            return search(root.left, targetValue);
+        }
+        else {
+            return true;
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+}
